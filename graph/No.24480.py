84e2d56a4c955f86b9107354e9b7d45bb1636a58
최현수 <zxqwas9510@kyungsung.ac.kr>
2023-09-12 12:18:05 +0900

    알고리즘 수업 - 깊이 우선 탐색 2

---
+++ graph/No.24480.py
@@ -0,0 +1,31 @@
+# 알고리즘 수업 - 깊이 우선 탐색 2
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+def dfs(R):
+    global k
+    visited[R] = k
+    for neighbor in edges[R]:
+        if not visited[neighbor] : 
+            k+=1
+            dfs(neighbor)
+
+N, M, R = map(int,input().split())
+k = 1
+# edges
+edges = [[] for _ in range(N+1)]
+# U -> V 로의 간선은 있는데 V -> U로의 간선은 없다는 것 처럼 되어 있어서 양방향으로 만들어줘야함
+for _ in range(M):
+    u,v = map(int,input().split())
+    edges[u].append(v)
+    edges[v].append(u)
+
+for i in range(N+1):
+    edges[i].sort(reverse= True)
+
+visited = [0] * (N+1)
+
+# R: 시작 정점
+dfs(R)
+for i in range(1,N+1):
+    print(visited[i])
