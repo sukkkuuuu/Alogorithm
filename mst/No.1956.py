9e37853d1e5f207ac7f4b8064ce73c0d60182c25
최현수 <zxqwas9510@kyungsung.ac.kr>
2023-09-13 15:33:20 +0900

    골목 대장 호석 - 효율성 1 수정

--- mst/No.1956.py
+++ mst/No.1956.py
@@ -18,33 +18,8 @@ for k in range(1, v+1):
 answer = sys.maxsize
 
 for i in range(1, v+1):
+    # 사이클은 출발지랑 도착지가 같기 떄문에 i->i를 비교해주면 된다
     answer = min(answer, graph[i][i])
 
 if answer == sys.maxsize: print(-1)
 else: print(answer)
-
-# V, E = map(int, input().split())
-# #거리를 저장할 matrix
-# matrix = [[int(1e9)] * (V+1) for _ in range(V+1)]
-# for _ in range(E):
-#     x, y, c = map(int, input().split())
-#     matrix[x][y] = c
-
-# #경유지 k, 출발지 i, 도착지 j 로 3중 for문을 돌면서
-# for k in range(1, V+1):
-#     for i in range(1, V+1):
-#         for j in range(1, V+1):
-#             # i->j 가 빠른지 i->k->j가 빠른지를 검사한다.
-#             if matrix[i][k] + matrix[k][j] < matrix[i][j]:
-#                 matrix[i][j] = matrix[i][k] + matrix[k][j]
-
-# answer = 1e9
-# for i in range(1, V+1):
-#    #사이클은 결국 출발지와 도착지가 같은 경우이므로 i->i를 확인
-#     answer = min(answer, matrix[i][i])
-# print(matrix)
-# #최소값이 없으면 -1, 있으면 최소값을 출력
-# if answer == 1e9:
-#     print(-1)
-# else:
-#     print(answer)
---
+++ mst/No.20182.py
@@ -0,0 +1,47 @@
+# 골목 대장 호석 - 효율성 1
+import sys, heapq
+input = sys.stdin.readline
+
+N, M, v1, v2, money = map(int,input().split())
+INF = sys.maxsize
+graph = [[] for _ in range(N+1)]
+# 내는 요금을 저장하는 배열
+costs = []
+for _ in range(M):
+    from_,to_,w = map(int,input().split())
+    graph[from_].append((to_,w))
+    graph[to_].append((from_,w))
+    costs.append(w)
+
+def dij(max_):
+    # 수금하면서 낸 요금 저장
+    distances = [INF] *(N+1)
+    visited = [False] *(N+1)
+    distances[v1] = 0
+    visited[v1] = True
+    pq = []
+    heapq.heappush(pq, [0, v1])
+
+    while pq:
+        cost, node = heapq.heappop(pq)
+        visited[cost] = True
+        if distances[node] < cost: continue
+
+        for next, ncost in graph[node]:
+            if distances[next] > ncost + cost and ncost <= max_:
+                # 간선 비용이 제한 비용 이내면 이동 가능
+                distances[next] = ncost + cost
+                heapq.heappush(pq, [ncost + cost, next])
+    return False
+#   사용 가능한 c원 이하로 b에 도착할 수 있다면 INF가 아닌 값 리턴
+
+costs.sort()
+L,R = -1, 21
+answer = INF
+while (L + 1 < R):
+    mid = (L+R) // 2
+    if (dij(mid)):
+        R = mid
+    else: L = mid
+if answer == INF:print(-1)
+else: print(answer)
